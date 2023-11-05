e06c7375c357902b8aed5b3338ddad2a45aeaf89
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-11-05 22:15:21 +0800

    streaming prototype

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,3 +1,23 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import axios from 'axios'
+
+export default async function handler(req, res) {
+  try{
+    const response = await axios.post(`${process.env.PYTHON_BACKEND_AI_URI}/web_query`, 
+    req.body.requestBody,
+    {responseType: 'stream',
+  })
+    res.status(200);
+    response.data.pipe(res)
+    response.data.on('end', () => {
+      console.log('Streaming completed.');
+      res.end();
+    })
+  } catch (error){
+    console.log(error)
+    res.status(500).end()
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function Home() {
 
   const [name, setName] = useState(null)
+  const [pythonMsg, setPythonMsg] = useState(null)
 
   useEffect(() => {
     async function getName() {
@@ -11,13 +12,23 @@ export default function Home() {
       console.log(response)
       setName(response.data.name)
     }
+
+    async function getPythonMsg(){
+      const response = await axios.get('/api/testpython')
+      console.log(response)
+      setPythonMsg(response.data.message)
+    }
     getName()
+    getPythonMsg()
   }, [])
+
+
 
   return (
     <>
       <p>Testing hello!</p>
       <p>API works! {name} says hello</p>
+      <p>Python Backend works! Python says {pythonMsg}</p>
     </>
   )
 }
