f171272c4c5bf733576a4f7019824242679cf2be
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-11-08 13:05:51 +0800

    added basic font family

---
+++ src/pages/dropdown/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from "react";
+import RightSideBar from "@/components/rightSideBar";
+
+
+export default function DropDown() {
+
+  return (
+    <div>
+      <RightSideBar/>
+    </div>
+  )
+}
