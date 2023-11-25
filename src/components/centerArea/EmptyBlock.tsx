21b5545d01df1d50d88daee7f4b7f67a46afea14
Morgan O'Sullivan <yongliang1000@gmail.com>
2023-11-24 21:57:48 -0800

    created empty block

---
+++ src/components/centerArea/EmptyBlock.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/styles/components/DynamicTextArea.module.css";
+
+function EmptyBlock() {
+  return <div className={styles.genericBlock}></div>;
+}
+
+export default EmptyBlock;
