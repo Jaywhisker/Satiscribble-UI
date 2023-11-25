b98dff992546faf2f51cbf1480ed2fe92da76fcc
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-11-25 12:25:12 +0800

    added api calls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,18 @@ import axios from "axios";
 import MeetingDetailBlocks from "@/components/centerArea/MeetingDetailBlocks";
 import TextAreaQuill from "@/components/centerArea/TextAreaUsingQuill";
 import RightSideBar from "@/components/rightSideBar";
+import CenterArea from "@/components/centerArea/CenterArea";
 
 export default function Home() {
-  const [name, setName] = useState(null);
-  const [pythonMsg, setPythonMsg] = useState(null);
+
   
-  const [topicAreas, setTopicareas] = useState([]);
-  
-  const handleAddTopicArea = () => {
-    setTopicareas((prevTopicAreas) => [...prevTopicAreas, {}]);
-  };
 
   return (
     <div style={{display:"flex", "flex-direction": "row"}}>
       <div style={{display: "flex", "flex-direction": "column", width:'15vw', backgroundColor: 'black', height:'100vh'}}>
         <p>Left Hand Side</p>
       </div>
-      <div style={{ display: "flex", "flex-direction": "column", width:'60vw' }}>
-          <MeetingDetailBlocks />
-          {topicAreas.map((_, index) => (
-            <TextAreaQuill
-              id={index}
-              key={index}
-              shouldFocus={index === topicAreas.length - 1}
-            />
-          ))}
-          <button onClick={handleAddTopicArea}>Add TopicArea</button>{" "}
-      </div>
-
+      <CenterArea/>
       <RightSideBar/>
     </div>
   );
