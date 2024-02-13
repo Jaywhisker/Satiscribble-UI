1e4d2d2e0346e9157b14f10d143ab6b0b914f475
Morgan O'Sullivan <yongliang1000@gmail.com>
2024-02-13 20:46:06 +0800

    Commented most of the  main area

--- src/components/centerArea/EmptyBlock.tsx
+++ src/components/centerArea/EmptyBlock.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "@/styles/components/DynamicTextArea.module.css";
 
 interface emptyBlockProps {
-  showCover: boolean; // New prop for cover visibility
+  showCover: boolean; // New prop for cover visibility, not in use cause there is nothing to cover
 }
 
 function EmptyBlock(props: emptyBlockProps) {
   return (
     <div className={styles.genericBlockHolder}>
-      {props.showCover && <div className={styles.emptyBlockCover}></div>}
+      {/* {props.showCover && <div className={styles.emptyBlockCover}></div>} */}
       <div
         className={styles.genericBlock}
         style={{ marginTop: "var(--firstBlockMarginFromTop)" }}
--- src/components/centerArea/MeetingDetailBlocks.tsx
+++ src/components/centerArea/MeetingDetailBlocks.tsx
@@ -11,58 +11,48 @@ interface meetingDetailBlockProps {
 }
 
 function MeetingDetailBlocks(props: meetingDetailBlockProps) {
-  const containerRef = useRef(null);
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false); //Used to check if the contents in the Meeting Details block has changed
   const [meetingFocused, setMeetingFocused] = useState(false);
 
+  //Reference Containers
+  const containerRef = useRef(null);
   const dateRef = useRef(null);
   const timeRef = useRef(null);
   const participantsRef = useRef(null);
   const locationRef = useRef(null);
   const listRef = [dateRef, timeRef, participantsRef, locationRef];
 
+  //Notification toasts
   const toast = useToast();
 
-  useEffect(() => {
-    // check if there is a meetingDetails alert fail
-    var meetingDetailsFailedAlert = toast.alertContainer.filter(
-      (alert) => alert.type === "meetingSaveFail" && alert.stateValue == false
-    );
-    if (meetingDetailsFailedAlert.length > 0) {
-      //if there is an error and meeting minutes in focus, remove alert
-      if (meetingFocused) {
-        toast.update(
-          meetingDetailsFailedAlert[0].id,
-          "meetingSaveFail",
-          true
-        );
-      }
-    }
-  }, [toast.alertContainer, meetingFocused]);
+  // ------------------------------------------------------------------------
+  //
+  //                        Updater Functions
+  //
+  // ------------------------------------------------------------------------
 
+  // Helper function that sets isEditing to true when contents change
   const handleInputChange = () => {
     setIsEditing(true);
   };
 
+  // Function that handles mouse clicks which decides when to update the meeting details backend
   const handleContainerClick = async (e) => {
+    // If one of the input areas were clicked
     if (containerRef.current && containerRef.current.contains(e.target)) {
-      //reference the error container at time of addEventListener...HELPS
       setMeetingFocused(true);
       return;
     }
+    // If meetingFocused (from mouse click) and content has changed
     if (isEditing && meetingFocused) {
+      // Data from the respective fields
+      // Date and time are not used  in the update info due to formating
       const dateValue = dateRef.current.value;
       const timeValue = timeRef.current.value;
       const participantsValue = participantsRef.current.value;
       const locationValue = locationRef.current.value;
-      setMeetingFocused(false);
 
-      // console.log("API call triggered");
-      // console.log("Date: " + dateValue);
-      // console.log("Time: " + timeValue);
-      // console.log("Participants: " + participantsValue);
-      // console.log("Location: " + locationValue);
-
+      // calls the update Meeting Details api to update the backend
       var response = await updateMeetingDetails(
         props.minutesID,
         props.chatHistoryID,
@@ -70,26 +60,21 @@ function MeetingDetailBlocks(props: meetingDetailBlockProps) {
         participantsValue
       );
       if (response !== undefined) {
-        //handle error
-        console.log("Meeting Details Error:", response.ERROR);
-        toast.meetingSaveFail(
-          false,
-          { location: locationValue, participants: participantsValue },
-          );
+        console.log("Meeting Details Error:", response.ERROR); // Sends console log if fail to update
+        toast.meetingSaveFail(false, {
+          location: locationValue,
+          participants: participantsValue,
+        }); //pushes toast notification on failure to update
       } else {
         setIsEditing(false);
+        setMeetingFocused(false);
+        // Put a console.log here if you want to ensure that the function is working
       }
     }
   };
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleContainerClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleContainerClick);
-    };
-  }, [isEditing, meetingFocused]);
-
+  // Function that allows for up and down to switch between the input areas
+  // Basically each area has an index and just uses that index that cycle
   function handleKeyDown(e, index) {
     if (e.key == "ArrowDown") {
       e.preventDefault();
@@ -104,6 +89,32 @@ function MeetingDetailBlocks(props: meetingDetailBlockProps) {
     }
   }
 
+  // Adds the mouse down listner to the document, making use of the function above
+  useEffect(() => {
+    document.addEventListener("mousedown", handleContainerClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleContainerClick);
+    };
+  }, [isEditing, meetingFocused]);
+
+  // Function that handles alerts
+  // If there is an error with updating the meeting details and the blcok is clicked
+  // Reomove the function
+  useEffect(() => {
+    // check if there is a meetingDetails alert fail
+    var meetingDetailsFailedAlert = toast.alertContainer.filter(
+      (alert) => alert.type === "meetingSaveFail" && alert.stateValue == false
+    );
+    if (meetingDetailsFailedAlert.length > 0) {
+      //if there is an error and meeting minutes in focus, remove alert
+      if (meetingFocused) {
+        toast.update(meetingDetailsFailedAlert[0].id, "meetingSaveFail", true);
+      }
+    }
+  }, [toast.alertContainer, meetingFocused]);
+
+  //HTML
   return (
     <div className={styles.genericBlockHolder}>
       {props.showCover && <div className={styles.genericBlockCover}></div>}
